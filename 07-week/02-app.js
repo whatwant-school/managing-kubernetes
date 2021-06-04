c6107899ae5f8023e86c43048c0391b6ab1b6564
whatwant <whatwant@gmail.com>
2021-06-04 21:54:31 +0900

    07-week 실습 파일 업로드

--- 07-week/02-app.js
+++ 07-week/02-app.js
@@ -1,12 +1,12 @@
 const http = require('http');
 const os = require('os');
 
-console.log("node-web server starting...");
+console.log("node-web v2 server starting...");
 
 var handler = function(request, response) {
   console.log("Received request from " + request.connection.remoteAddress);
   response.writeHead(200);
-  response.end("You've hit " + os.hostname() + "\n");
+  response.end("You've hit " + os.hostname() + " for v2\n");
 };
 
 var www = http.createServer(handler);
